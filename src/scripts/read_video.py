6fcec8f232b9cc5fadebc399baec0133a57d928d
ahmed255324 <ahmed-noman3@outlook.com>
2021-11-27 20:36:03 +0100

    read_video.py mit fehler

--- src/scripts/read_video.py
+++ src/scripts/read_video.py
@@ -5,7 +5,7 @@ import cv2
 video_capture1 = cv2.VideoCapture(0)
 video_capture2 = cv2.VideoCapture(1)
 video_capture3 = cv2.VideoCapture(2)
-#video_capture = cv2.VideoCapture('video/ros.mp4')
+#video_capture = cv2.VideoCapture('video/ros.mp')
 
 while(True):
 	ret1, frame1 = video_capture1.read()
