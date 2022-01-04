bbd64bf8808804b3a25900f4a48439feaf227284
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-04 22:20:49 +0100

    -m

--- src/scripts/Auswertung/P_geschwindigkeit_regler.py
+++ src/scripts/Auswertung/P_geschwindigkeit_regler.py
@@ -4,10 +4,10 @@ import rospy
 from sensor_msgs.msg import Joy
 import sys
 
-def geschwindigkeit_regler(geschwindigkeit, lenkung):
+def geschwindigkeit_regler():
     joy_pub = Joy()
-    joy_pub.axes[1] = geschwindigkeit
-    joy_pub.axes[3] = lenkung
+    joy_pub.axes[1] = 0.1
+    joy_pub.axes[3] = 0.6
     rospy.init_node('P_geschwindigkeit_regler', anonymous=True)
     pub = rospy.Publisher('/joy', Joy, queue_size=1)
     rate = rospy.Rate(10) # 10 hz
@@ -18,6 +18,7 @@ def geschwindigkeit_regler(geschwindigkeit, lenkung):
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("Die Nutzung von geschwindigkeit_regler.py mit defoalt arg1 arg2")
+        #print("Die Nutzung von geschwindigkeit_regler.py mit defoalt arg1 arg2")
+        geschwindigkeit_regler()
     else:
-        geschwindigkeit_regler(sys.argv[1], sys.argv[2])
+        geschwindigkeit_regler()
