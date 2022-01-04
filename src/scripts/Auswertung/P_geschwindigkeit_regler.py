f099a59640e2dad8cb78f795cd9f41563cddab5e
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-04 22:54:25 +0100

    -m

--- src/scripts/Auswertung/P_geschwindigkeit_regler.py
+++ src/scripts/Auswertung/P_geschwindigkeit_regler.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import Joy
+from Sensorik_Team8_PKG.msg import movecontrol
 import sys
 
 def geschwindigkeit_regler(geschwindigkeit, lenkung):
-    joy_pub = Joy()
-    joy_pub.axes = [0.0, geschwindigkeit, 0.0, lenkung, 0.0, 0.0]
-
+    steuerung = movecontrol()
+    steuerung.geschwindigkeit = geschwindigkeit
+    steuerung.lenkung = lenkung
     rospy.init_node("P_geschwindigkeit_regler", anonymous=True)
-    pub = rospy.Publisher("/joy", Joy, queue_size=1)
-    rate = rospy.Rate(10) # 10 hz
+    pub = rospy.Publisher("/control", movecontrol, queue_size=1)
+    rate = rospy.Rate(1) # 10 hz
     #keep publishing until a Ctrl-C is pressed
     while not rospy.is_shutdown():
-        pub.publish(joy_pub)
+        pub.publish(steuerung)
         rate.sleep()
+    steuerung.geschwindigkeit = 0.0
+    steuerung.lenkung = 0.0
+    pub.publish(steuerung)
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
