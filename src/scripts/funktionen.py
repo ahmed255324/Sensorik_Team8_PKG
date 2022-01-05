8203b5e26b2a9dc0353b03271a69314fcf442faa
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-05 09:52:49 +0100

    -m

--- src/scripts/funktionen.py
+++ src/scripts/funktionen.py
@@ -9,6 +9,17 @@ def isRotationMatrix(R):
     n = np.linalg.norm(I - shouldBeIdentity)
     return n < 1e-6
 
+# Checks if a matrix is a valid rotation matrix.
+def isRotationMatrix(R) :
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype = R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+# Calculates rotation matrix to euler angles
+# The result is the same as MATLAB except the order
+# of the euler angles ( x and z are swapped ).
 
 def TF(rvecs, tvecs):
 	tf = np.zeros((4,4), dtype= float)
@@ -31,3 +42,4 @@ def rotationMatrixToEulerAngles(R) :
         y = math.atan2(-R[2,0], sy)
         z = 0
     return np.array([x, y, z])
+
