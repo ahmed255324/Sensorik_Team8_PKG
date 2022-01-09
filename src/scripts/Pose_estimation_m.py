6ac9da7cd080ca4ef73b3fe2d97fda8baa0efc9f
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-09 15:50:55 +0100

    -m

--- src/scripts/Pose_estimation_m.py
+++ src/scripts/Pose_estimation_m.py
@@ -136,7 +136,7 @@ while(not rospy.is_shutdown()):
 		M1 = tf[0:3, 0:3]
 		eulerW = funktionen.eulerAnglesToRotationMatrix(M1)
 
-		mittelwertw = mittelwertw + eulerW[2].strip 
+		mittelwertw = mittelwertw + eulerW[2].asscalar()
 		pose_a.Z = mittelwert/teiler
 		# Quaternion
 		if((float(1)+M1[0,0]+M1[1,1]+M1[2,2]) > 0 ):
