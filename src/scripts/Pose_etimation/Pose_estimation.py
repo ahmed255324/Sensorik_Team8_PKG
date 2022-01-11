8f59e7a3b1a1813548d9b3692e7554198b7a9bfb
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-11 13:49:44 +0100

    -m

--- src/scripts/Pose_etimation/Pose_estimation.py
+++ src/scripts/Pose_etimation/Pose_estimation.py
@@ -20,9 +20,9 @@ pose_a = auswertungsmessage()
 empty_message = Empty()
 pose_o.model_name = "unit_box"
 
-video_capture1 = cv2.VideoCapture(0, cv2.CAP_V4L2)
+video_capture3 = cv2.VideoCapture(0, cv2.CAP_V4L2)
 video_capture2 = cv2.VideoCapture(2, cv2.CAP_V4L2)
-video_capture3 = cv2.VideoCapture(4, cv2.CAP_V4L2)
+video_capture1 = cv2.VideoCapture(4, cv2.CAP_V4L2)
 
 a = 190
 
