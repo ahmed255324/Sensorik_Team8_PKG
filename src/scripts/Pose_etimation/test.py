8a0f62beb3a4a0e73a6e8ba6955e34550527a957
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-27 10:51:28 +0100

    -m

--- src/scripts/Pose_etimation/test.py
+++ src/scripts/Pose_etimation/test.py
@@ -4,6 +4,21 @@ from _thread import start_new_thread
 import threading
 from pyzbar.pyzbar import decode
 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+# allow the camera to warmup
+time.sleep(0.1)
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+# display the image on screen and wait for a keypress
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
 def heron(a):
     video_capture = cv2.VideoCapture(a, cv2.CAP_V4L2)
     if not video_capture.isOpened():
