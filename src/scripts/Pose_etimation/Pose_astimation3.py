faefcabcf5921dfc4e631066f30a06839c81d64e
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-28 07:49:29 +0100

    -m

--- src/scripts/Pose_etimation/Pose_astimation3.py
+++ src/scripts/Pose_etimation/Pose_astimation3.py
@@ -21,6 +21,14 @@ empty_message = Empty()
 pose_o.model_name = "unit_box"
 
 video_capture1 = cv2.VideoCapture(0, cv2.CAP_V4L2)
+video_capture2 = cv2.VideoCapture(2, cv2.CAP_V4L2)
+video_capture3 = cv2.VideoCapture(4, cv2.CAP_V4L2)
+
+video_capture1.release()
+video_capture2.release()
+video_capture3.release()
+
+video_capture1 = cv2.VideoCapture(0, cv2.CAP_V4L2)
 fps = int(video_capture1.get(5))
 print("fps:", fps)
 
@@ -28,12 +36,18 @@ video_capture2 = cv2.VideoCapture(2, cv2.CAP_V4L2)
 fps = int(video_capture2.get(5))
 print("fps:", fps)
 
+video_capture3 = cv2.VideoCapture(4, cv2.CAP_V4L2)
+fps = int(video_capture3.get(5))
+print("fps:", fps)
+
 a = 190
 cameraMatrix_1 = np.genfromtxt("/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_1/cameraMatrix_1.csv", delimiter=',')
 cameraMatrix_2 = np.genfromtxt("/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_2/cameraMatrix_2.csv", delimiter=',')
+cameraMatrix_3 = np.genfromtxt("/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_3/cameraMatrix_3.csv", delimiter=',')
 
 dist_1 = np.genfromtxt('/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_1/dist_1.csv', delimiter=',')
 dist_2 = np.genfromtxt('/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_2/dist_2.csv', delimiter=',')
+dist_3 = np.genfromtxt('/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_3/dist_3.csv', delimiter=',')
 
 objectPoints = np.random.random((4,3,1))
 imagePoints = np.random.random((4,2,1))
@@ -48,9 +62,11 @@ objectPoints[3] = [[a/2], [a/2], [0]]
 
 tf_1 = np.zeros((4,4))	
 tf_2 = np.zeros((4,4))
+tf_3 = np.zeros((4,4))
 tf= np.zeros((4,4))
 
 while(not rospy.is_shutdown()):
+
 	ret1, frame1 = video_capture1.read()
 	code1 = decode(frame1)
 	for qrcode1 in code1:
@@ -81,19 +97,46 @@ while(not rospy.is_shutdown()):
 		tf_2 = np.dot(tf_2, [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
 		print('2')
 		
+	ret3, frame3 = video_capture3.read()
+	code3 = decode(frame3)
+	for qrcode3 in code3:
+		barcodeData_3 = qrcode3.data.decode("utf-8")
+		points = np.array(code3[0].polygon, np.int32)
+		imagePoints[0] = [[points[0][0]], [points[0][1]]]
+		imagePoints[1] = [[points[1][0]], [points[1][1]]]
+		imagePoints[2] = [[points[2][0]], [points[2][1]]]
+		imagePoints[3] = [[points[3][0]], [points[3][1]]]
+		_, rvecs_3, tvecs_3 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_3, dist_3, flags=cv2.SOLVEPNP_P3P)
+		tf_3 = funktionen.TF(rvecs=rvecs_3, tvecs=tvecs_3)
+		tf_3 = np.dot(tabelle.qrcode_tf[int(barcodeData_3)-1], tf_3)
+		tf_3 = np.dot(tf_3, [[0, 1, 0, 0], [0, 0, 1, 0], [1, 0, 0, -0.1], [0, 0, 0, 1]])
+		print('3')
+
 	if(code1):
 		if(code2):
 			tf = (tf_1 + tf_2)/2
+		if(code3):
+			tf = (tf_1 + tf_3)/2
 		else:
 			tf = tf_1
 		
 	elif(code2):
 		if(code1):
 			tf = (tf_1 + tf_2)/2
+		elif(code3):
+			tf = (tf_2 + tf_3)/2
 		else:
 			tf = tf_2
 
-	if(code1 or code2):	
+	elif(code3):
+		if(code1):
+			tf = (tf_1 + tf_3)/2
+		elif(code2):
+			tf = (tf_2 + tf_3)/2
+		else:
+			tf = tf_3
+
+	if(code1 or code2 or code3):	
 		pose_o.pose.position.x = tf[0][3] 
 		pose_o.pose.position.y = tf[1][3]
 		pose_o.pose.position.z = 0
