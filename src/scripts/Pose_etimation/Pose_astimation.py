3b53c14a2110fad065e7ff3560c30373147787d9
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-29 14:25:19 +0100

    -m

--- src/scripts/Pose_etimation/Pose_astimation.py
+++ src/scripts/Pose_etimation/Pose_astimation.py
@@ -29,6 +29,10 @@ fps = int(video_capture2.get(5))
 print("fps:", fps)
 
 a = 190
+
+vergleichwert_qd = 0.0
+vergleichwert_w = 0.0
+
 cameraMatrix_1 = np.genfromtxt("/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_1/cameraMatrix_1.csv", delimiter=',')
 cameraMatrix_2 = np.genfromtxt("/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_2/cameraMatrix_2.csv", delimiter=',')
 
@@ -48,9 +52,48 @@ objectPoints[3] = [[a/2], [a/2], [0]]
 
 tf_1 = np.zeros((4,4))	
 tf_2 = np.zeros((4,4))
-tf= np.zeros((4,4))
+tf	 = np.zeros((4,4))
 
+for i in range(1, 20):
+	ret1, frame1 = video_capture1.read()
+	code1 = decode(frame1)
+	for qrcode1 in code1:
+		barcodeData_1 = qrcode1.data.decode("utf-8")
+		points = np.array(code1[0].polygon, np.int32)
+		imagePoints[0] = [[points[0][0]], [points[0][1]]]
+		imagePoints[1] = [[points[1][0]], [points[1][1]]]
+		imagePoints[2] = [[points[2][0]], [points[2][1]]]
+		imagePoints[3] = [[points[3][0]], [points[3][1]]]
+		_, rvecs_1, tvecs_1 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_1, dist_1, flags=cv2.SOLVEPNP_P3P)
+		tf_1 = funktionen.TF(rvecs=rvecs_1, tvecs=tvecs_1)
+		tf_1 = np.dot(tabelle.qrcode_tf[int(barcodeData_1)-1], tf_1)
+		tf_1 = np.dot(tf_1, [[-1, 0, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]])
 
+	ret2, frame2 = video_capture2.read()
+	code2 = decode(frame2)
+	for qrcode2 in code2:
+		barcodeData_2 = qrcode2.data.decode("utf-8")
+		points = np.array(code2[0].polygon, np.int32)
+		imagePoints[0] = [[points[0][0]], [points[0][1]]]
+		imagePoints[1] = [[points[1][0]], [points[1][1]]]
+		imagePoints[2] = [[points[2][0]], [points[2][1]]]
+		imagePoints[3] = [[points[3][0]], [points[3][1]]]
+		_, rvecs_2, tvecs_2 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_2, dist_2, flags=cv2.SOLVEPNP_P3P)
+		tf_2 = funktionen.TF(rvecs=rvecs_2, tvecs=tvecs_2)
+		tf_2 = np.dot(tabelle.qrcode_tf[int(barcodeData_2)-1], tf_2)
+		tf_2 = np.dot(tf_2, [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
+		
+	if(code1):
+		tf = tf_1
+		
+	elif(code2):
+		tf = tf_2
+
+	if(code1 or code2):	
+		vergleichwert_qd  = vergleichwert_qd + tf[1][3]
+			
+vergleichwert_qd = vergleichwert_qd / 20
+print (vergleichwert_qd)
 while(not rospy.is_shutdown()):
 	ret1, frame1 = video_capture1.read()
 	code1 = decode(frame1)
@@ -79,21 +122,19 @@ while(not rospy.is_shutdown()):
 		tf_2 = funktionen.TF(rvecs=rvecs_2, tvecs=tvecs_2)
 		tf_2 = np.dot(tabelle.qrcode_tf[int(barcodeData_2)-1], tf_2)
 		tf_2 = np.dot(tf_2, [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
-		print('2')
 		
 	if(code1):
-		if(code2):
-			tf = (tf_1 + tf_2)/2
-		else:
-			tf = tf_1
+		tf = tf_1
 		
 	elif(code2):
-		if(code1):
-			tf = (tf_1 + tf_2)/2
-		else:
-			tf = tf_2
+		tf = tf_2
 
 	if(code1 or code2):	
+		if abs(vergleichwert_qd - tf[1][3]) > 0.15:
+			tf[1][3] = vergleichwert_qd
+		else:
+			vergleichwert_qd = tf[1][3]
+
 		pose_o.pose.position.x = tf[0][3] 
 		pose_o.pose.position.y = tf[1][3]
 		pose_o.pose.position.z = 0
@@ -101,7 +142,6 @@ while(not rospy.is_shutdown()):
 		pose_a.X = tf[0][3]
 		pose_a.Y = tf[1][3]
 		M1 = tf[0:3, 0:3]
-
 		eulerW = funktionen.eulerAnglesToRotationMatrix(M1)			
 		pose_a.Z = eulerW[2]*(180/pi)
 		# Quaternion
