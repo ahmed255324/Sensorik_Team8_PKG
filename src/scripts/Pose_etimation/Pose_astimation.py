b810e6fa76a50adb6549e0fe2c60af3f8277dd90
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-29 19:45:20 +0100

    -m

--- src/scripts/Pose_etimation/Pose_astimation.py
+++ src/scripts/Pose_etimation/Pose_astimation.py
@@ -20,47 +20,31 @@ pose_a = auswertungsmessage()
 empty_message = Empty()
 pose_o.model_name = "unit_box"
 
-video_capture1 = cv2.VideoCapture(0, cv2.CAP_V4L2)
-fps = int(video_capture1.get(5))
+video_capture2 = cv2.VideoCapture(0, cv2.CAP_V4L2)
+fps = int(video_capture2.get(5))
 print("fps:", fps)
 
-video_capture2 = cv2.VideoCapture(2, cv2.CAP_V4L2)
-fps = int(video_capture2.get(5))
+video_capture3 = cv2.VideoCapture(2, cv2.CAP_V4L2)
+fps = int(video_capture3.get(5))
 print("fps:", fps)
 
 a = 190
 
-vergleichwert_qd = 0.0
-vergleichwert_w = 0.0
-
-cameraMatrix_1 = np.genfromtxt("/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_1/cameraMatrix_1.csv", delimiter=',')
 cameraMatrix_2 = np.genfromtxt("/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_2/cameraMatrix_2.csv", delimiter=',')
+cameraMatrix_3 = np.genfromtxt("/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_3/cameraMatrix_3.csv", delimiter=',')
 
-dist_1 = np.genfromtxt('/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_1/dist_1.csv', delimiter=',')
 dist_2 = np.genfromtxt('/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_2/dist_2.csv', delimiter=',')
+dist_3 = np.genfromtxt('/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_3/dist_3.csv', delimiter=',')
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objectPoints = np.array([[-a/2, a/2, 0], [-a/2, -a/2, 0], [a/2, -a/2, 0], [a/2, a/2, 0]], dtype=np.float32)
 objectPoints = np.reshape(objectPoints, (4,3,1))
-
-tf_1 = np.zeros((4,4))	
+	
 tf_2 = np.zeros((4,4))
-tf	 = np.zeros((4,4))
+tf_3 = np.zeros((4,4))
 
 
 while(not rospy.is_shutdown()):
-	ret1, frame1 = video_capture1.read()
-	code1 = decode(frame1)
-	for qrcode1 in code1:
-		barcodeData_1 = qrcode1.data.decode("utf-8")
-		points = np.array(code1[0].polygon, np.float32)
-		if((4,2) == np.shape(points)):
-			imagePoints = np.reshape(points, (4,2,1))
-			_, rvecs_1, tvecs_1 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_1, dist_1, flags=cv2.SOLVEPNP_P3P)
-			tf_1 = funktionen.TF(rvecs=rvecs_1, tvecs=tvecs_1)
-			tf_1 = np.dot(tabelle.qrcode_tf[int(barcodeData_1)-1], tf_1)
-			tf_1 = np.dot(tf_1, [[-1, 0, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]])
-
 	ret2, frame2 = video_capture2.read()
 	code2 = decode(frame2)
 	for qrcode2 in code2:
@@ -72,21 +56,27 @@ while(not rospy.is_shutdown()):
 			tf_2 = funktionen.TF(rvecs=rvecs_2, tvecs=tvecs_2)
 			tf_2 = np.dot(tabelle.qrcode_tf[int(barcodeData_2)-1], tf_2)
 			tf_2 = np.dot(tf_2, [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
-	
-	if(code1):
-		tf = tf_1
-		
-	elif(code2):
-		tf = tf_2
 
-	if(code1 or code2):	
-		pose_o.pose.position.x = tf[0][3] 
-		pose_o.pose.position.y = tf[1][3]
+	ret3, frame3 = video_capture3.read()
+	code3 = decode(frame3)
+	for qrcode3 in code3:
+		barcodeData_3 = qrcode3.data.decode("utf-8")
+		points = np.array(code3[0].polygon, np.int32)
+		if((4,2) == np.shape(points)):
+			imagePoints = np.reshape(points, (4,2,1))
+			_, rvecs_3, tvecs_3 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_3, dist_3, flags=cv2.SOLVEPNP_P3P)
+			tf_3 = funktionen.TF(rvecs=rvecs_3, tvecs=tvecs_3)
+			tf_3 = np.dot(tabelle.qrcode_tf[int(barcodeData_3)-1], tf_3)
+			tf_3 = np.dot(tf_3, [[0, 1, 0, 0], [0, 0, 1, 0], [1, 0, 0, -0.1], [0, 0, 0, 1]])
+	if(code3 or code2):
+
+		pose_o.pose.position.x = tf_3[0][3] 
+		pose_o.pose.position.y = tf_2[1][3]
 		pose_o.pose.position.z = 0
-		print(tf[0][3], tf[1][3])
-		pose_a.X = tf[0][3]
-		pose_a.Y = tf[1][3]
-		M1 = tf[0:3, 0:3]
+		print(tf_3[0][3], tf_2[1][3])
+		pose_a.X = tf_3[0][3]
+		pose_a.Y = tf_2[1][3]
+		M1 = tf_2[0:3, 0:3]
 		eulerW = funktionen.eulerAnglesToRotationMatrix(M1)			
 		pose_a.Z = eulerW[2]*(180/pi)
 		# Quaternion
@@ -104,5 +94,5 @@ while(not rospy.is_shutdown()):
 		puba.publish(pose_a)
 		pubm.publish(empty_message)
 
-video_capture1.release()
+video_capture3.release()
 video_capture2.release()
