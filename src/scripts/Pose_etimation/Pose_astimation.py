de31d95ce49a79f8ff987d07f5547a95fde78a2a
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-29 14:55:56 +0100

    -m

--- src/scripts/Pose_etimation/Pose_astimation.py
+++ src/scripts/Pose_etimation/Pose_astimation.py
@@ -40,7 +40,7 @@ dist_1 = np.genfromtxt('/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/script
 dist_2 = np.genfromtxt('/home/ubuntu/catkin_ws/src/Sensorik_Team8_PKG/src/scripts/Usb_cam_calabration_2/dist_2.csv', delimiter=',')
 
 objectPoints = np.random.random((4,3,1))
-imagePoints = np.random.random((4,2,1))
+imagePoints = np.zeros((4,2,1,))
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
@@ -100,11 +100,10 @@ while(not rospy.is_shutdown()):
 	for qrcode1 in code1:
 		barcodeData_1 = qrcode1.data.decode("utf-8")
 		points = np.array(code1[0].polygon, np.int32)
-		if(points.all()):
-			imagePoints[0] = [[points[0][0]], [points[0][1]]]
-			imagePoints[1] = [[points[1][0]], [points[1][1]]]
-			imagePoints[2] = [[points[2][0]], [points[2][1]]]
-			imagePoints[3] = [[points[3][0]], [points[3][1]]]
+		imagePoints[0] = [[points[0][0]], [points[0][1]]]
+		imagePoints[1] = [[points[1][0]], [points[1][1]]]
+		imagePoints[2] = [[points[2][0]], [points[2][1]]]
+		imagePoints[3] = [[points[3][0]], [points[3][1]]]
 		_, rvecs_1, tvecs_1 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_1, dist_1, flags=cv2.SOLVEPNP_P3P)
 		tf_1 = funktionen.TF(rvecs=rvecs_1, tvecs=tvecs_1)
 		tf_1 = np.dot(tabelle.qrcode_tf[int(barcodeData_1)-1], tf_1)
@@ -115,11 +114,11 @@ while(not rospy.is_shutdown()):
 	for qrcode2 in code2:
 		barcodeData_2 = qrcode2.data.decode("utf-8")
 		points = np.array(code2[0].polygon, np.int32)
-		if(points.all()):
-			imagePoints[0] = [[points[0][0]], [points[0][1]]]
-			imagePoints[1] = [[points[1][0]], [points[1][1]]]
-			imagePoints[2] = [[points[2][0]], [points[2][1]]]
-			imagePoints[3] = [[points[3][0]], [points[3][1]]]
+
+		imagePoints[0] = [[points[0][0]], [points[0][1]]]
+		imagePoints[1] = [[points[1][0]], [points[1][1]]]
+		imagePoints[2] = [[points[2][0]], [points[2][1]]]
+		imagePoints[3] = [[points[3][0]], [points[3][1]]]
 		flag_2, rvecs_2, tvecs_2 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_2, dist_2, flags=cv2.SOLVEPNP_P3P)
 		print(flag_2)
 		tf_2 = funktionen.TF(rvecs=rvecs_2, tvecs=tvecs_2)
