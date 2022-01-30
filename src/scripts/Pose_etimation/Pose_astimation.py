71484634c9b84e885de168787dc9f533aac11c7f
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-30 15:14:37 +0100

    -m

--- src/scripts/Pose_etimation/Pose_astimation.py
+++ src/scripts/Pose_etimation/Pose_astimation.py
@@ -57,7 +57,7 @@ while(not rospy.is_shutdown()):
 		if((4,2) == np.shape(points)):
 			imagePoints = np.reshape(points, (4,2,1))
 			flag_2, rvecs_2, tvecs_2 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_2, dist_2, flags=cv2.SOLVEPNP_P3P)
-			type(tvecs_2)
+			print(type(tvecs_2))
 			if(int(barcodeData_2) > 1 and int(barcodeData_2) < 20):
 
 				x = tabelle.qrcode_tf[int(barcodeData_2)-1][0][3] + tvecs_2[2]
@@ -74,7 +74,7 @@ while(not rospy.is_shutdown()):
 		if((4,2) == np.shape(points)):
 			imagePoints = np.reshape(points, (4,2,1))
 			_, rvecs_3, tvecs_3 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_3, dist_3, flags=cv2.SOLVEPNP_P3P)
-			type(tvecs_3)
+			print(type(tvecs_3))
 			if(int(barcodeData_3) > 1 and int(barcodeData_3) < 20):
 				y = tabelle.qrcode_tf[int(barcodeData_3)-1][1][3] + tvecs_3[2]
 			else:
