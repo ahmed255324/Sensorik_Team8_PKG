6a7ae0c67c3ad326195cae349c190b500b1130d0
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-30 19:10:52 +0100

    -m

--- src/scripts/Pose_etimation/Pose_astimation.py
+++ src/scripts/Pose_etimation/Pose_astimation.py
@@ -59,15 +59,16 @@ while(not rospy.is_shutdown()):
 		if((4,2) == np.shape(points)):
 			imagePoints = np.reshape(points, (4,2,1))
 			flag_2, rvecs_2, tvecs_2 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_2, dist_2, flags=cv2.SOLVEPNP_P3P)
-			print(tvecs_2)
-			tf_2 = funktionen.TF(rvecs=rvecs_2, tvecs=tvecs_2)
-			tf_2 = np.dot(tabelle.qrcode_tf[int(barcodeData_2)-1], tf_2)
-			tf_2 = np.dot(tf_2, [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
-			#print(tf_2)
-			if(int(barcodeData_2) >= 1 and int(barcodeData_2) < 20):
-				y = tf_2[0][3]
-			else:
-				x = tf_2[1][3]
+			if(flag_2):
+				print(tvecs_2)
+				tf_2 = funktionen.TF(rvecs=rvecs_2, tvecs=tvecs_2)
+				tf_2 = np.dot(tabelle.qrcode_tf[int(barcodeData_2)-1], tf_2)
+				tf_2 = np.dot(tf_2, [[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
+				#print(tf_2)
+				if(int(barcodeData_2) >= 1 and int(barcodeData_2) < 20):
+					y = tf_2[0][3]
+				else:
+					x = tf_2[1][3]
 		win = win + int(barcodeData_2) * cam_2
 
 
