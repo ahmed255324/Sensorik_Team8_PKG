d237a6673034fcbe55e22b420fdbb19c31f9b091
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-30 20:31:08 +0100

    -m

--- src/scripts/Pose_etimation/Pose_astimation.py
+++ src/scripts/Pose_etimation/Pose_astimation.py
@@ -58,7 +58,7 @@ while(not rospy.is_shutdown()):
 		points = np.array(code2[0].polygon, np.float32)
 		if((4,2) == np.shape(points)):
 			imagePoints = np.reshape(points, (4,2,1))
-			_, rvecs_2, tvecs_2 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_2, dist_2, flags=cv2.SOLVEPNP_P3P)
+			_, _, tvecs_2 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_2, dist_2, flags=cv2.SOLVEPNP_P3P)
 			if tvecs_2 is not None:
 				if(barcodeData_2 >= 1 and barcodeData_2 < 20):
 					y = tabelle.qrcode_tf[barcodeData_2-1][1][3] + tvecs_2[2]/1000
@@ -76,17 +76,12 @@ while(not rospy.is_shutdown()):
 		if((4,2) == np.shape(points)):
 			#print(barcodeData_3)
 			imagePoints = np.reshape(points, (4,2,1))
-			_, rvecs_3, tvecs_3 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_3, dist_3, flags=cv2.SOLVEPNP_P3P)
-			
+			_, _, tvecs_3 = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix_3, dist_3, flags=cv2.SOLVEPNP_P3P)
 			if tvecs_3 is not None:
-				#print(tvecs_3)
-				#tf_3 = funktionen.TF(rvecs=rvecs_3, tvecs=tvecs_3)
-				#tf_3 = np.dot(tabelle.qrcode_tf[int(barcodeData_3)-1], tf_3)
-				#tf_3 = np.dot(tf_3, [[0, 1, 0, 0], [0, 0, 1, 0], [1, 0, 0, -0.1], [0, 0, 0, 1]])
 				if(int(barcodeData_3) >= 1 and int(barcodeData_3) < 20):
-					x = tf_3[1][3]
+					x = tabelle.qrcode_tf[barcodeData_3-1][0][3] + tvecs_3[2]/1000
 				else:
-					y = tf_3[1][3]
+					y = tabelle.qrcode_tf[barcodeData_3-1][1][3] + tvecs_3[2]/1000
 		win = win + int(barcodeData_3) * cam_3
 
 	if(code3 or code2):
