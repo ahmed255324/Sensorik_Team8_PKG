9b4388d409a262508de1d52e8a1135bd1f8fabe7
ahmed255324 <ahmed-noman3@outlook.com>
2022-01-31 17:42:42 +0100

    -m

--- src/scripts/Pose_etimation/Pose_astimation.py
+++ src/scripts/Pose_etimation/Pose_astimation.py
@@ -62,10 +62,9 @@ while(not rospy.is_shutdown()):
 			if tvecs_2 is not None:
 				tf_2[2] = tf_2[2] + 0.1
 				tf_2 = np.dot(tabelle.qrcode_tf[barcodeData_2-1], funktionen.TF(tvecs_2))
-				print(2)
 				if(barcodeData_2 > 7 and barcodeData_2 < 24):
 					x = tf_2[0][3]
-					print(2)
+					print('camera', 3)
 				else:
 					y = tf_2[1][3]
 				win = win + barcodeData_2 * cam_2
@@ -82,7 +81,7 @@ while(not rospy.is_shutdown()):
 			if tvecs_3 is not None:
 				tf_3[2] = tf_3[2] + 0.1
 				tf_3 = np.dot(tabelle.qrcode_tf[barcodeData_3-1], funktionen.TF(tvecs_3))
-				print(3)
+				print('camera', 3)
 				if(barcodeData_3 > 7 and barcodeData_3 < 24):
 					x = tf_3[0][3]
 					print(3)
